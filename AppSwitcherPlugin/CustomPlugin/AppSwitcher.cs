2d65338da1ff7efcf31df7f491df3f66a72b06ee
Hristo Stoyanov <renegat96@gmail.com>
2012-01-25 11:40:59 +0200

    AppSwitcherPlugin initial upload

---
+++ AppSwitcherPlugin/CustomPlugin/AppSwitcher.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace ModernSteward
+{
+    public class AppSwitcher
+    {
+        public void SwitchAhead(int number)
+        {
+            KeyboardSend.KeyDown(Keys.Alt);
+            for (int i = 0; i < number; ++i)
+            {
+                KeyboardSend.KeyDown(Keys.Tab);
+                KeyboardSend.KeyUp(Keys.Tab);
+            }
+            KeyboardSend.KeyUp(Keys.Alt);
+        }
+        public void SwitchToApp(int number)
+        {
+            try
+            {
+                KeyboardSend.KeyDown(Keys.LWin);
+                KeyboardSend.KeyDown(NumToKey(number));
+                KeyboardSend.KeyUp(Keys.LWin);
+                KeyboardSend.KeyUp(NumToKey(number));
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                throw new ArgumentOutOfRangeException("AppSwitcher.SwitchToApp() method was invoked with invalid argument", ex);
+            }
+        }
+        private Keys NumToKey(int number)
+        {
+            switch (number)
+            {
+                case 1: return Keys.D1;
+                case 2: return Keys.D2;
+                case 3: return Keys.D3;
+                case 4: return Keys.D4;
+                case 5: return Keys.D5;
+                case 6: return Keys.D6;
+                case 7: return Keys.D7;
+                case 8: return Keys.D8;
+                case 9: return Keys.D9;
+                default:
+                    throw new ArgumentOutOfRangeException("AppSwitcher.NumToKey() method was invoked with invalid argument");
+            }
+        }
+    }
+}
---
+++ AppSwitcherPlugin/CustomPlugin/CustomPlugin.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ModernSteward;
+using System.Speech.Recognition;
+using System.Reflection;
+using System.IO;
+
+namespace ModernSteward
+{
+    public class CustomPlugin : PluginFunctionality
+    {
+        public override void Trigger(List<KeyValuePair<string, string>> semanticsToDict)
+        {
+            AppSwitcher switcher = new AppSwitcher();
+			if(semanticsToDict[0].Key == "switch to"){
+				if(semanticsToDict[1].Key == "first application"){
+                    switcher.SwitchToApp(1);
+				}
+				if(semanticsToDict[1].Key == "second application"){
+                    switcher.SwitchToApp(2);
+				}
+				if(semanticsToDict[1].Key == "third application"){
+                    switcher.SwitchToApp(3);
+				}
+				if(semanticsToDict[1].Key == "fourth application"){
+                    switcher.SwitchToApp(4);
+				}
+				if(semanticsToDict[1].Key == "fifth application"){
+                    switcher.SwitchToApp(5);
+				}
+				if(semanticsToDict[1].Key == "sixth application"){
+                    switcher.SwitchToApp(6);
+				}
+				if(semanticsToDict[1].Key == "seventh application"){
+                    switcher.SwitchToApp(7);
+				}
+				if(semanticsToDict[1].Key == "eighth application"){
+                    switcher.SwitchToApp(8);
+				}
+				if(semanticsToDict[1].Key == "nineth application"){
+                    switcher.SwitchToApp(9);
+				}
+			}
+			if(semanticsToDict[0].Key == "next application"){
+                switcher.SwitchAhead(1);
+			}
+
+        }
+
+        public override GrammarBuilder GetGrammarBuilder()
+        {
+            Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("ModernSteward.CustomPluginGrammar.xml");
+            return ModernSteward.TreeViewToGrammarBuilderAlgorithm.CreateGrammarBuilderFromXML(stream);
+        }
+
+        public override void Initialize()
+        {
+            //Custom initialization for your plugin
+        }
+    }
+}
