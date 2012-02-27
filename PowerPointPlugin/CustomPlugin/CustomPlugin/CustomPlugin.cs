b44d6e0afad2cad60fef67c23e20e1ada3b1fe8e
Hristo Stoyanov <renegat96@gmail.com>
2012-02-27 02:52:19 +0200

    PowerPointPlugin grammar improvement and presentation chosing fix

--- PowerPointPlugin/CustomPlugin/CustomPlugin/CustomPlugin.cs
+++ PowerPointPlugin/CustomPlugin/CustomPlugin/CustomPlugin.cs
@@ -17,41 +17,34 @@ namespace ModernSteward
 
         public override void Trigger(List<KeyValuePair<string, string>> semanticsToDict)
         {
-			if (semanticsToDict[0].Key == "power point")
+			if (semanticsToDict[0].Key == "next slide")
             {
-				if (semanticsToDict[1].Key == "next slide")
+                if (null == slideshow)
                 {
-                    if (null == slideshow)
-                    {
-                        //TODO: exception handling. Discuss with Liubo about message delivery system.
-                    }
-                    slideshow.NextSlide();
-				}
-				if (semanticsToDict[1].Key == "previous slide")
-                {
-                    if (null == slideshow)
-                    {
-                        //TODO: see "next slide case"
-                    }
-                    slideshow.PreviousSlide();
-				}
-				if (semanticsToDict[1].Key == "close presentation")
-                {
-                    if (null == slideshow)
-                    {
-                        //TODO: see "next slide case"
-                    }
-                    slideshow.ClosePresentation();
-				}
-				if (semanticsToDict[1].Key == "open presentation")
-                {
-					if (semanticsToDict[2].Key == "PresentationName")
-                    {
-						string dictationForPresentationName = semanticsToDict[2].Value;
-                        slideshow = new SlideshowManager(dictationForPresentationName);
-					}
-				}
+                    //TODO: exception handling. Discuss with Liubo about message delivery system.
+                }
+                slideshow.NextSlide();
 			}
+			if (semanticsToDict[0].Key == "previous slide")
+            {
+                if (null == slideshow)
+                {
+                    //TODO: see "next slide case"
+                }
+                slideshow.PreviousSlide();
+			}
+			if (semanticsToDict[0].Key == "close presentation")
+            {
+                if (null == slideshow)
+                {
+                    //TODO: see "next slide case"
+                }
+                slideshow.ClosePresentation();
+			}
+            if (semanticsToDict[0].Key == "open presentation")
+            {
+                slideshow = new SlideshowManager(PresentationFilePath);
+            }
 
         }
 
@@ -72,7 +65,8 @@ namespace ModernSteward
         public override bool Initialize()
         {
 			InitializationForm init = new InitializationForm();
-			init.Show();
+			init.ShowDialog();
+            this.PresentationFilePath = init.PresentationFilePath;
             return true;
         }
     }
