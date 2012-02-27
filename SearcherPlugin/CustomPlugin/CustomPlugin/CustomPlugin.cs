014d063a479add0d4cbd58162491bc411b3516d4
Hristo Stoyanov <renegat96@gmail.com>
2012-02-27 03:38:28 +0200

    SearcherPlugin rebuild

--- SearcherPlugin/CustomPlugin/CustomPlugin/CustomPlugin.cs
+++ SearcherPlugin/CustomPlugin/CustomPlugin/CustomPlugin.cs
@@ -14,27 +14,60 @@ namespace ModernSteward
         public override void Trigger(List<KeyValuePair<string, string>> semanticsToDict)
         {
             
-			if (semanticsToDict[0].Key == "search in") {
+			try {
+				if (semanticsToDict[0].Key == "search in"){
 
-				if (semanticsToDict[1].Key == "google for") {
-					if (semanticsToDict[2].Key == "Keywords") {
-						string dictationForKeywords = semanticsToDict[2].Value;
-                        System.Diagnostics.Process.Start("http://www.google.com/#q=" + dictationForKeywords);
+					try {
+						if (semanticsToDict[2].Key == "movie database for"){
+
+							try {
+								if (semanticsToDict[4].Key == "Keywords"){
+									string dictationForKeywords = semanticsToDict[4].Value;
+
+                                    System.Diagnostics.Process.Start("http://www.imdb.com/find?s=all&q=" + dictationForKeywords);
+								}
+							}
+							catch (Exception e) { }
+
+						}
 					}
-				}
-				if (semanticsToDict[1].Key == "wikipedia for") {
-					if (semanticsToDict[2].Key == "Keywords") {
-						string dictationForKeywords = semanticsToDict[2].Value;
-                        System.Diagnostics.Process.Start("http://en.wikipedia.com/wiki/index.php?title=Special%3ASearch&button=&search=" + dictationForKeywords);
+					catch (Exception e) { }
+
+					try {
+						if (semanticsToDict[2].Key == "google for"){
+
+							try {
+								if (semanticsToDict[4].Key == "Keywords"){
+									string dictationForKeywords = semanticsToDict[4].Value;
+                                    System.Diagnostics.Process.Start("http://www.google.com/#q=" + dictationForKeywords);
+								}
+							}
+							catch (Exception e) { }
+
+						}
 					}
-				}
-				if (semanticsToDict[1].Key == "movie database for") {
-					if (semanticsToDict[2].Key == "Keywords") {
-						string dictationForKeywords = semanticsToDict[2].Value;
-                        System.Diagnostics.Process.Start("http://www.imdb.com/find?s=all&q=" + dictationForKeywords);
+					catch (Exception e) { }
+
+					try {
+						if (semanticsToDict[2].Key == "wikipedia for"){
+
+							try {
+								if (semanticsToDict[4].Key == "Keywords"){
+									string dictationForKeywords = semanticsToDict[4].Value;
+
+                                    System.Diagnostics.Process.Start("http://en.wikipedia.com/wiki/index.php?title=Special%3ASearch&button=&search=" + dictationForKeywords);
+								}
+							}
+							catch (Exception e) { }
+
+						}
 					}
+					catch (Exception e) { }
+
 				}
 			}
+			catch (Exception e) { }
+
 
         }
 
@@ -46,7 +79,7 @@ namespace ModernSteward
 
         public override bool Initialize()
         {
-            //Custom initialization for your plugin
+            //Returns true if the initialization of the plugin was successfull
             return true;
         }
     }
